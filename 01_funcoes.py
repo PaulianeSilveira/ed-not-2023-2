b3e826200a5083701029a7e73a7f65a7efce237b
PAULIANE ESTER SILVEIRA <1091392113038@fatec-srv.local>
2023-08-14 22:11:57 -0300

    (14/08) Início de listas

--- 01_funcoes.py
+++ 01_funcoes.py
@@ -1,4 +1,4 @@
-#Fiunção que calcula o IMC de uma pessoa
+#Função que calcula o IMC de uma pessoa
 def calc_imc(peso, altura): 
     return peso / altura ** 2
 
@@ -9,3 +9,33 @@ def calc_imc(peso, altura):
 # print(imc)
 
 print(calc_imc(85, 1.72))
+
+#########################################
+
+"""Comentário - Docstring - serve para documentar funções e classes - aceita vária linhas """
+
+from math import pi
+
+"""Função que calcula a área de uma figura geométrica plana"""
+
+def calc_area(base, altura, tipo):
+    # resultado = None #undefined no JS - valor não existente
+    if tipo == "R": # Retângulo  = atribuição; == comparação
+        resultado = base * altura
+    elif tipo == "T": #Triângulo
+        resultado = base * altura /2
+    elif tipo == "E": #Elipse
+        resultado = (base/2) * (altura / 2) * pi
+    else: #Forma inválida ou desconhecida
+        resultado = None
+    return resultado 
+
+print("Retângulo 20x30", calc_area(20,30, "R"))
+print("Triângulo 45x32", calc_area(45,32, "T"))
+print("Elipse 10x15", calc_area(10,15, "E"))
+print("Desconhecido 12x16", calc_area(12,16, "X"))
+
+
+
+
+
